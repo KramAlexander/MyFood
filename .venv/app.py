407f28c144a4ff0a01f42e4be91e2dafb9767b7c
Alexander Kram <alexander.kram@gmx.de>
2024-12-11 01:13:55 +0100

    add basic recipe html and css

--- .venv/app.py
+++ .venv/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index_html')
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
