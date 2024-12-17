1b617048a0239ebebd591459a8aead312170eee8
Alexander Kram <alexander.kram@gmx.de>
2024-12-17 21:14:24 +0100

    add example of restapi for user

--- .venv/app.py
+++ .venv/app.py
@@ -20,12 +20,59 @@ user_args = reqparse.RequestParser()
 user_args.add_argument('name',type=str,required=True,help="Name cannot be blank")
 user_args.add_argument('email',type=str,required=True,help="Email cannot be blank")
 
+userFields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'email': fields.String,
+}
+
 class Users(Resource):
+    @marshal_with(userFields)
     def get(self):
         users = UserModel.query.all()
         return users
     
+    @marshal_with(userFields)
+    def post(self):
+        args = user_args.parse_args()
+        user = UserModel(name=args["name"], email=args["email"])
+        db.session.add(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users, 201
+    
+class User(Resource):
+    @marshal_with(userFields)
+    def get(self,id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,"0User not found")
+        return user
+    
+    @marshal_with(userFields)
+    def patch(self,id):
+        args = user_args.parse_args()
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,"0User not found")
+        user.name = args["name"]
+        user.email = args["email"]
+        db.session.commit()
+        return user
+    
+    @marshal_with(userFields)
+    def delete(self,id):
+        user = UserModel.query.filter_by(id=id).first()
+        if not user:
+            abort(404,"0User not found")
+        db.session.delete(user)
+        db.session.commit()
+        users = UserModel.query.all()
+        return users
+    
 api.add_resource(Users, '/api/users/')
+api.add_resource(User,'/api/users/<int:id>')
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -35,4 +82,5 @@ def index():
 def test():
     return '<h1>test api call</h1>'
 if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=8000) # running on localhost, port 8000
+    app.run(debug=True)
+#serve(app, host="0.0.0.0", port=8000) # running on localhost, port 8000
